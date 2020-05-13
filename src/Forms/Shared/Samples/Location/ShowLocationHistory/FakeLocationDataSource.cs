882357de23a76b40ed8c555ab6fd6d60caa83585
Thusjanthan Kubendranathan <thusjanthan.kubendranathan@bmo.com>
2020-05-13 15:31:47 -0400

    - Fake Location Data Source - Updated Show Location to use the fake loation data source - Updated panning mode to navigation

---
+++ src/Forms/Shared/Samples/Location/ShowLocationHistory/FakeLocationDataSource.cs
@@ -0,0 +1,169 @@
+﻿// Copyright 2019 Esri.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at: http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an 
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific 
+// language governing permissions and limitations under the License.
+
+using Esri.ArcGISRuntime.Geometry;
+using Esri.ArcGISRuntime.Location;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ArcGISRuntimeXamarin.Samples.ShowLocationHistory
+{
+    class FakeLocationDataSource : LocationDataSource
+    {
+        public FakeLocationDataSource()
+        {
+            // Generate the points that will be used.
+
+            // The location will walk around a circle around the point.
+            Polygon outerCircle = (Polygon)GeometryEngine.BufferGeodetic(_circleRouteCenter, 1000, LinearUnits.Feet);
+
+            // Get a list of points on the circle from the buffered point.
+            _artificialMapPoints = outerCircle.Parts[0].Points.ToList();
+        }
+
+        ~FakeLocationDataSource()
+        {
+            if (IsStarted)
+            {
+                OnStopAsync().Start();
+            }
+        }
+
+        private NmeaParser.NmeaFileDevice fileDevice = null;
+
+        // Center around which the fake locations are centered.
+        private readonly MapPoint _circleRouteCenter = new MapPoint(-117.195801, 34.056007, SpatialReferences.Wgs84);
+
+        // List of points around the circle for use in generating fake locations.
+        private readonly List<MapPoint> _artificialMapPoints;
+
+        // Index keeps track of where on the fake track you are.
+        private int _locationIndex = 0;
+
+        private volatile object stateLock = new object();
+
+        private bool isStarted = false;
+
+        protected override Task OnStartAsync()
+        {
+            lock (stateLock)
+            {
+                if (!isStarted)
+                {
+                    isStarted = true;
+
+                    string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "GPS_V_Log.txt");
+
+                    fileDevice = new NmeaParser.NmeaFileDevice(filePath, 400);
+                    fileDevice.MessageReceived += OnMessageReceived;
+
+                    return fileDevice.OpenAsync();
+                }
+                return Task.CompletedTask;
+            }
+        }
+
+        protected override Task OnStopAsync()
+        {
+            lock (stateLock)
+            {
+                if (isStarted)
+                {
+                    isStarted = false;
+                    if (fileDevice != null)
+                    {
+                        Task.Run(async () => await fileDevice.CloseAsync()).Wait();
+                        fileDevice.MessageReceived -= OnMessageReceived;
+                        fileDevice?.Dispose();
+                        fileDevice = null;
+                    }
+                }
+                return Task.CompletedTask;
+            }
+        }
+
+
+        public int TrackedSatelliteCount { get; protected set; } = 0;
+        protected double Course { get; set; } = 0;
+        protected double Velocity { get; set; } = 0;
+
+
+        protected virtual void OnMessageReceived(object sender, NmeaParser.NmeaMessageReceivedEventArgs args)
+        {
+            const double knotsToMeterPerSecond = 0.5144;
+
+            try
+            {
+                double? latitude = null;
+                double? longitude = null;
+
+                switch (args.Message)
+                {
+                    case NmeaParser.Messages.Gga gga:
+                        TrackedSatelliteCount = gga.NumberOfSatellites;             // Satellites being tracked
+                                                                                    // gga.Hdop                                                 // Horizontal dilution of position
+                        if (gga.Quality != NmeaParser.Messages.Gga.FixQuality.Invalid)
+                        {
+                            latitude = gga.Latitude;
+                            longitude = gga.Longitude;
+                        }
+                        break;
+
+                    case NmeaParser.Messages.Gsa gsa:
+                        TrackedSatelliteCount = (gsa.SatelliteIDs?.Count() ?? 0);   // Satellite IDs used for fix
+                                                                                    // gsa.Fix                                                  // None; 2D; or, 3D
+                                                                                    // gsa.Pdop
+                                                                                    // gsa.Hdop
+                                                                                    // gsa.Vdop
+                        break;
+
+                    case NmeaParser.Messages.Rma rma:
+                        if (rma.Status != NmeaParser.Messages.Rma.PositioningStatus.Invalid)
+                        {
+                            latitude = rma.Latitude;
+                            longitude = rma.Longitude;
+                            if (!double.IsNaN(rma.Course))
+                            {
+                                Course = rma.Course;
+                            }
+                            Velocity = (rma.Speed * knotsToMeterPerSecond);
+                        }
+                        break;
+
+                    case NmeaParser.Messages.Rmc rmc:
+                        latitude = rmc.Latitude;
+                        longitude = rmc.Longitude;
+                        if (!double.IsNaN(rmc.Course))
+                        {
+                            Course = rmc.Course;
+                        }
+                        Velocity = (rmc.Speed * knotsToMeterPerSecond);
+                        break;
+                }
+
+                var validLatitude = ((latitude != null) && (!double.IsNaN(latitude.Value)));
+                var validLongitude = ((longitude != null) && (!double.IsNaN(longitude.Value)));
+
+                if (validLatitude && validLongitude)
+                {
+                    var mapPoint = new MapPoint(longitude.Value, latitude.Value, SpatialReferences.Wgs84);
+                    var location = new Esri.ArcGISRuntime.Location.Location(mapPoint, 2, Velocity, Course, false);
+                    UpdateLocation(location);
+                }
+            }
+            catch (Exception ex)
+            {
+                
+            }
+        }
+    }
+}
--- src/Forms/Shared/Samples/Location/ShowLocationHistory/readme.md
+++ src/Forms/Shared/Samples/Location/ShowLocationHistory/readme.md
@@ -2,7 +2,7 @@
 
 Display your location history on the map.
 
-![Image of show location history](ShowLocationHistory.jpg)
+![](ShowLocationHistory.jpg)
 
 ## Use case
 
@@ -10,16 +10,15 @@ You can track device location history and display it as lines and points on the 
 
 ## How to use the sample
 
-Tap 'Start tracking' to start tracking your location, which will appear as points on the map. A line will connect the points for easier visualization. Tap 'Stop tracking' to stop updating the location history. This sample uses a simulated data source to allow the sample to be useful on desktop/non-mobile devices. To track a user's real position, use the `SystemLocationDataSource` instead.
+Click, 'Start tracking' to start tracking your location, which will appear as points on the map. A line will connect the points for easier visualization. Click 'Stop tracking' to stop updating the location history.
 
 ## How it works
 
 1. If necessary, request location permission from the operating system.
 2. Create a graphics overlay to show each point and another graphics overlay for displaying the route line.
-3. Create a `SimulatedLocationDataSource` and initialize it with a polyline. Start the `SimulatedLocationDataSource` to begin receiving location updates.
- - NOTE: To track a user's real position, use `SystemLocationDataSource` instead.
+3. Create a `SystemLocationDataSource` and start it to begin receiving location updates.
 4. Subscribe to the `LocationChanged` event to handle location updates.
-5. Every time the location updates, store that location, display a point on the map, and recreate the route line.
+5. Every time the location updates, store that location, display a point on the map, and re-create the route line.
 
 ## Relevant API
 
@@ -31,12 +30,12 @@ Tap 'Start tracking' to start tracking your location, which will appear as point
 * LocationDisplay.DataSource
 * LocationDisplay.IsEnabled
 * MapView.LocationDisplay
-* SimulatedLocationDataSource
+* SystemLocationDataSource
 
 ## About the data
 
-A custom set of points is used to create a `Polyline` and initialize a `SimulatedLocationDataSource`. This simulated location data source enables easier testing and allows the sample to be used on devices without an actively updating GPS signal.
+The sample uses a dark gray basemap with lime and red graphics. A custom location data source that provides fake/random location data is provided to enable easier testing and allow the sample to be useful on desktop/non-mobile devices.
 
 ## Tags
 
-bread crumb, breadcrumb, GPS, history, movement, navigation, real-time, trace, track, trail
+GPS, bread crumb, breadcrumb, history, movement, navigation, real-time, trace, track, trail
